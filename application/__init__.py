9612e1debf8ffe210bbba36ef2d038bc47362fe6
Malik Piara <malikpiara@gmail.com>
2022-06-17 16:30:38 -0500

    Connected app to MongoDB. Showing real users in people page.

--- application/__init__.py
+++ application/__init__.py
@@ -1,8 +1,55 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask, render_template
+from flask_pymongo import PyMongo
+from werkzeug.security import generate_password_hash
 
+load_dotenv()
 app = Flask(__name__)
 
-users = ['Malik Piara', 'John Snow', 'Clark Kent']
+
+class Config(object):
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+
+    # PyMongo Configuration
+    app.config["MONGO_URI"] = os.environ.get("MONGODB_URI")
+    mongo = PyMongo(app)
+
+
+app.config.from_object(Config)
+
+db = Config.mongo.db
+
+
+# Models
+def create_user(email_address, first_name, last_name, password):
+    hashed_pass = generate_password_hash(
+        password)
+    db.users.insert_one(
+        {
+            "email": email_address,
+            "first_name": first_name,
+            "last_name": last_name,
+            "password": hashed_pass,
+            "account_status": "active",
+        })
+
+
+def get_users():
+    users = []
+    for user in db.users.find(
+        {
+            "_id": {"$exists": True}
+        }
+    ):
+        entry = {
+            "_id": user["_id"],
+            "first_name": user["first_name"],
+            "last_name": user["last_name"]
+        }
+
+        users.append(entry)
+    return users
 
 
 @app.route("/")
@@ -22,4 +69,5 @@ def login():
 
 @app.route("/people")
 def people():
+    users = get_users()
     return render_template('people.html', users=users)
