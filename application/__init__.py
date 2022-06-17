bb23aec09350423ca5b80b69dd924bd97e30fcb7
Malik Piara <malikpiara@gmail.com>
2022-06-16 21:39:33 -0500

    Hello World

---
+++ application/__init__.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def hello_world():
+    return render_template('hello.html')
+
+
+@app.route("/join")
+def signup():
+    return render_template('signup.html')
+
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
